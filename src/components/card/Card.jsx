c20b254ae9423dc75c998af51f06a09bdc699bc9
dimtirisafendras <dimitris.afendras@gmail.com>
2020-03-16 19:28:56 +0200

    progress on card added indicator

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Card.css';
+
+export const Card = () => {
+	return (
+		<>
+			<div className='Card1'/>
+			<div className='Card2'>
+				<div className='Icon'/>
+				<div className='TextContainer'/>
+			</div>
+			<div className='Card3'>
+				<div className='Icon2'/>
+				<div className='TextContainer2'>
+					<div className='Text'/>
+					<div className='Amount'/>
+				</div>
+			</div>
+			<div className='Card4'>
+				<div className='Icon3'/>
+				<div className='TextContainer3'>
+					<div className='Text2'/>
+					<div className='Amount2'/>
+				</div>
+			</div>
+			<div className='Card5'>
+
+				<div className='Icon4'/>
+				<div className='TextContainer4'>
+					<div className='InnerTextContainer'>
+						<div className='InnerText'/>
+						<div className='InnerNumber'/>
+					</div>
+					<div className='Amount3'/>
+				</div>
+			</div>
+			<div className='Card6'>
+				<div className='Indicator'/>
+				<div className='Icon5'/>
+				<div className='TextContainer5'>
+					<div className='InnerTextContainer2'>
+						<div className='InnerText2'/>
+						<div className='InnerNumber2'/>
+					</div>
+					<div className='Amount4'/>
+				</div>
+			</div>
+		</>
+	);
+};
